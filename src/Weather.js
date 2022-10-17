9ba700baef2e262cc7e960d5925a2e2d4eeaf591
Dashasoham <dikrain8@gmail.com>
2022-10-17 15:52:41 +0100

    added alert

---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+export default function Weather(props) {
+  function showTemperature(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+  let apiKey = '8531f7780be3a3039a0314e6c276f900';
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showTemperature);
+
+  return <h1>Hi</h1>;
+}
