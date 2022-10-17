b03ff0b7564ce172a144e594318ffab40365a8b6
Dashasoham <dikrain8@gmail.com>
2022-10-17 17:00:00 +0100

    moved from Sandbox

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,85 @@
 import React from 'react';
-import axios from 'axios';
 
-export default function Weather(props) {
-  function showTemperature(response) {
-    alert(
-      `Hello, The weather in ${response.data.name} is ${response.data.main.temp}`
-    );
-  }
-  let apiKey = '8531f7780be3a3039a0314e6c276f900';
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showTemperature);
+import './Weather.css';
 
-  return <h1>Hi</h1>;
+export default function Weather() {
+  let weatherData = {
+    city: 'Athens',
+    temperature: 19,
+    date: 'Tuesday 10:00',
+    description: 'Sunny',
+    imgUrl: 'https://ssl.gstatic.com/onebox/weather/64/sunny.png',
+    humidity: 80,
+    wind: 10,
+  };
+  return (
+    <div className="Weather">
+      <div className="container">
+        <div className="app-wrapper">
+          <form className="mb-5">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city.."
+                  className="form-control"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <div className="row">
+            <div className="col-4">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-right"
+              />
+
+              <div>
+                <ul>
+                  <li>{weatherData.date}</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="current-city">
+                <h1>
+                  <div>{weatherData.city}</div>
+                </h1>
+                <br />
+                <span className="units">{weatherData.temperature}°C</span>
+                <ul>
+                  <li></li>
+                  <li>
+                    Humidity: <strong>{weatherData.humidity}%</strong> <br />
+                    Wind:
+                    <strong>{weatherData.wind}</strong>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-4"></div>
+          </div>
+        </div>
+      </div>
+      <p className="footer">
+        Coded by Daria Korniienko. This project is open-sourced on
+        <a
+          href="https://github.com/Dashasoham/Weather-Project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
