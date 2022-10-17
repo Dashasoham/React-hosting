51a8fbf399d685c519ee185dca37f92c49613a01
Dashasoham <dikrain8@gmail.com>
2022-10-17 15:54:54 +0100

    Alert the weather

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 
 export default function Weather(props) {
   function showTemperature(response) {
-    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+    alert(
+      `Hello, The weather in ${response.data.name} is ${response.data.main.temp}`
+    );
   }
   let apiKey = '8531f7780be3a3039a0314e6c276f900';
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
